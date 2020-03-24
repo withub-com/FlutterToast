90c86449287abf3310b9243a1e74c62bf4791e92
Poker <1040182715@qq.com>
2020-03-24 10:59:26 +0800

    移除androidx 修改buildsdk版本为26

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\ponna\Desktop\sdks\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ponna\Desktop\code\My\FlutterToast\example"
+export "FLUTTER_ROOT=D:\FlutterSDK\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\code\flutterPu\FlutterToast\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\ponna\Desktop\sdks\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\FlutterSDK\flutter\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
